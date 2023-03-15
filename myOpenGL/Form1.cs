caddbb080d357c17e2199c01a294fd3c614e04ff
chen <toaff100@gmail.com>
2023-03-15 21:41:15 +0200

    inital

---
+++ myOpenGL/Form1.cs
@@ -0,0 +1,282 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using OpenGL;
+using System.Runtime.InteropServices;
+
+namespace myOpenGL
+{
+    public partial class Form1 : Form
+    {
+        cOGL cGL;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            cGL = new cOGL(panel1);
+            //apply the bars values as cGL.ScrollValue[..] properties 
+            //!!!
+            //!!!
+            hScrollBarScroll(hScrollBar1, null);
+            hScrollBarScroll(hScrollBar2, null);
+            hScrollBarScroll(hScrollBar3, null);
+            hScrollBarScroll(hScrollBar4, null);
+            hScrollBarScroll(hScrollBar5, null);
+            hScrollBarScroll(hScrollBar6, null);
+            hScrollBarScroll(hScrollBar7, null);
+            hScrollBarScroll(hScrollBar8, null);
+            hScrollBarScroll(hScrollBar9, null);
+            //hScrollBarScroll(hScrollBar10, null);
+            hScrollBarScroll(hScrollBar11, null);
+            hScrollBarScroll(hScrollBar12, null);
+            hScrollBarScroll(hScrollBar13, null);
+            //hScrollBarScroll(hScrollBar14, null);
+        }
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            cGL.Draw();
+        }
+
+        private void panel1_Resize(object sender, EventArgs e)
+        {
+            //cGL.OnResize();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hScrollBarScroll(object sender, ScrollEventArgs e)
+        {
+            cGL.intOptionC = 0;
+            HScrollBar hb = (HScrollBar)sender;
+            int n = int.Parse(hb.Name.Substring(10));
+            cGL.ScrollValue[n - 1] = (hb.Value - 100) / 10.0f;
+            if (e != null)
+                cGL.Draw();
+        }
+
+        public float[] oldPos = new float[7];
+
+        private void numericUpDownValueChanged(object sender, EventArgs e)
+        {
+            NumericUpDown nUD = (NumericUpDown)sender;
+            int i = int.Parse(nUD.Name.Substring(nUD.Name.Length - 1));
+            int pos = (int)nUD.Value;
+            switch (i)
+            {
+                case 1:
+                    if (pos > oldPos[i - 1])
+                    {
+                        cGL.xShift += 0.25f;
+                        cGL.intOptionC = 4;
+                    }
+                    else
+                    {
+                        cGL.xShift -= 0.25f;
+                        cGL.intOptionC = -4;
+                    }
+                    break;
+                case 2:
+                    if (pos > oldPos[i - 1])
+                    {
+                        cGL.yShift += 0.25f;
+                        cGL.intOptionC = 5;
+                    }
+                    else
+                    {
+                        cGL.yShift -= 0.25f;
+                        cGL.intOptionC = -5;
+                    }
+                    break;
+                case 3:
+                    if (pos > oldPos[i - 1])
+                    {
+                        cGL.zShift += 0.25f;
+                        cGL.intOptionC = 6;
+                    }
+                    else
+                    {
+                        cGL.zShift -= 0.25f;
+                        cGL.intOptionC = -6;
+                    }
+                    break;
+                case 4:
+                    if (pos > oldPos[i - 1])
+                    {
+                        cGL.xAngle += 5;
+                        cGL.intOptionC = 1;
+                    }
+                    else
+                    {
+                        cGL.xAngle -= 5;
+                        cGL.intOptionC = -1;
+                    }
+                    break;
+                case 5:
+                    if (pos > oldPos[i - 1])
+                    {
+                        cGL.yAngle += 5;
+                        cGL.intOptionC = 2;
+                    }
+                    else
+                    {
+                        cGL.yAngle -= 5;
+                        cGL.intOptionC = -2;
+                    }
+                    break;
+                case 6:
+                    if (pos > oldPos[i - 1])
+                    {
+                        cGL.zAngle += 5;
+                        cGL.intOptionC = 3;
+                    }
+                    else
+                    {
+                        cGL.zAngle -= 5;
+                        cGL.intOptionC = -3;
+                    }
+                    break;
+            }
+            cGL.Draw();
+            oldPos[i - 1] = pos;
+            cGL.intOptionC = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            cGL.alpha -= 15;
+            cGL.Draw();
+        }
+
+
+
+        private void rotate_top(object sender, EventArgs e)
+        {
+
+
+            bool right = true;
+            if (sender == top_left)
+                right = false;
+
+            int rotate_direction = right ? 1 : -1;
+
+            for (int i = 0; i < 120; i++)
+            {
+                GL.glPushMatrix();
+                double[] rotation_matrix = new double[16];
+                double[] temp = new double[16];
+                GL.glLoadIdentity();
+                GL.glRotated(rotate_direction, Rubik_Management.dir_XYZ[Rubik_Management.axis - 1, 0], Rubik_Management.dir_XYZ[Rubik_Management.axis - 1, 1], Rubik_Management.dir_XYZ[Rubik_Management.axis - 1, 2]);
+                GL.glGetDoublev(GL.GL_MODELVIEW_MATRIX, temp);
+                GL.glMultMatrixd(Rubik_Management.t[Rubik_Management.top].rotation_matrix);
+                GL.glGetDoublev(GL.GL_MODELVIEW_MATRIX, rotation_matrix);
+
+                Rubik_Management.t[Rubik_Management.top].rotation_matrix = (double[])rotation_matrix.Clone();
+                GL.glPopMatrix();
+                cGL.Draw();
+
+            }
+        }
+
+            
+           
+
+        
+        private void rotate_middle(object sender, EventArgs e)
+        {
+            bool right = true;
+            if (sender == middle_left)
+                right = false;
+            int rotate_direction = right ? 1 : -1;
+
+            for (int j = 0; j < 120; j++)
+            {
+                for (int i = 0; i < Rubik_Management.t.Length; i++)
+                {
+                    if(Rubik_Management.t[i].loc == location.middle)
+                    {
+                        //Rubik_Management.t[i].rotation_in_dir[Rubik_Management.axis - 1] += 1 * rotate_direction;
+                        GL.glPushMatrix();
+                        double[] rotation_matrix = new double[16];
+                        double[] temp = new double[16];
+                        GL.glLoadIdentity();
+                        GL.glRotated(rotate_direction, Rubik_Management.dir_XYZ[Rubik_Management.axis - 1, 0], Rubik_Management.dir_XYZ[Rubik_Management.axis - 1, 1], Rubik_Management.dir_XYZ[Rubik_Management.axis - 1, 2]);
+                        GL.glGetDoublev(GL.GL_MODELVIEW_MATRIX, temp);
+                        GL.glMultMatrixd(Rubik_Management.t[i].rotation_matrix);
+                        GL.glGetDoublev(GL.GL_MODELVIEW_MATRIX, rotation_matrix);
+
+                        Rubik_Management.t[i].rotation_matrix = (double[])rotation_matrix.Clone();
+                        GL.glPopMatrix();
+                    }
+
+                }
+                cGL.Draw();
+            }
+            Rubik_Management.rotate_middle(right);
+
+        }
+        private void rotate_bottom(object sender, EventArgs e)
+        {
+            bool right = true;
+            if (sender == bottom_left)
+                right = false;
+            int rotate_direction = right ? 1 : -1;
+
+            for (int j = 0; j < 120; j++)
+            {
+                for (int i = 0; i < Rubik_Management.t.Length; i++)
+                {
+                    if (Rubik_Management.t[i].loc == location.bottom)
+                    {
+                        GL.glPushMatrix();
+                        double[] rotation_matrix = new double[16];
+                        double[] temp = new double[16];
+                        GL.glLoadIdentity();
+                        GL.glRotated(rotate_direction, Rubik_Management.dir_XYZ[Rubik_Management.axis - 1, 0], Rubik_Management.dir_XYZ[Rubik_Management.axis - 1, 1], Rubik_Management.dir_XYZ[Rubik_Management.axis - 1, 2]);
+                        GL.glGetDoublev(GL.GL_MODELVIEW_MATRIX, temp);
+                        //GL.glLoadMatrixd(Rubik_Management.t[i].rotation_matrix);
+                        GL.glMultMatrixd(Rubik_Management.t[i].rotation_matrix);
+                        GL.glGetDoublev(GL.GL_MODELVIEW_MATRIX, rotation_matrix);
+
+                        Rubik_Management.t[i].rotation_matrix = (double[])rotation_matrix.Clone();
+                        GL.glPopMatrix();
+                    }
+                }
+                cGL.Draw();
+            }
+            Rubik_Management.rotate_bottom(right);
+        }
+        private void direction_radioButton(object sender, EventArgs e)
+        {
+            if (sender == direction1)
+            {
+               Rubik_Management.set_direction(1);
+            }
+            else if(sender == direction2)
+            {
+                Rubik_Management.set_direction(2);
+            }
+            else if (sender == direction3)
+            {
+                Rubik_Management.set_direction(3);
+            }
+            else if (sender == direction4)
+            {
+                Rubik_Management.set_direction(4);
+            }
+
+        }
+
+      
+    }
+}
+        
---
+++ myOpenGL/cOGL.cs
@@ -0,0 +1,268 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+//2
+using System.Drawing;
+
+namespace OpenGL
+{
+    class cOGL
+    {
+        Control p;
+        int Width;
+        int Height;
+        public float alpha;
+        GLUquadric obj;
+
+        public cOGL(Control pb)
+        {
+            p = pb;
+            Width = p.Width;
+            Height = p.Height;
+            InitializeGL();
+            obj = GLU.gluNewQuadric(); //!!!
+            Rubik.init_Rubik();
+            Cubemap.GenerateTextures();
+        }
+
+        ~cOGL()
+        {
+            GLU.gluDeleteQuadric(obj); //!!!
+            WGL.wglDeleteContext(m_uint_RC);
+        }
+
+        uint m_uint_HWND = 0;
+
+        public uint HWND
+        {
+            get { return m_uint_HWND; }
+        }
+
+        uint m_uint_DC = 0;
+
+        public uint DC
+        {
+            get { return m_uint_DC; }
+        }
+        uint m_uint_RC = 0;
+
+        public uint RC
+        {
+            get { return m_uint_RC; }
+        }        
+
+        public float[] ScrollValue = new float[14];
+        public float zShift = 0.0f;
+        public float yShift = 0.0f;
+        public float xShift = 0.0f;
+        public float zAngle = 0.0f;
+        public float yAngle = 0.0f;
+        public float xAngle = 0.0f;
+        public float ARM_ANGLE = 0.0f;
+
+        public int intOptionC = 0;
+        double[] AccumulatedRotationsTraslations = new double[16];
+        public float[] pos = new float[4];
+
+        public void Draw()
+        {
+            pos[0] = ScrollValue[10];
+            pos[1] = ScrollValue[11];
+            pos[2] = ScrollValue[12];
+            pos[3] = 1.0f;
+
+            if (m_uint_DC == 0 || m_uint_RC == 0)
+                return;
+
+            GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT | GL.GL_STENCIL_BUFFER_BIT);
+
+            GL.glLoadIdentity();
+
+            // not trivial
+            double[] ModelVievMatrixBeforeSpecificTransforms = new double[16];
+            double[] CurrentRotationTraslation = new double[16];
+
+            GLU.gluLookAt(ScrollValue[0], ScrollValue[1], ScrollValue[2],
+                       ScrollValue[3], ScrollValue[4], ScrollValue[5],
+                       ScrollValue[6], ScrollValue[7], ScrollValue[8]);
+            GL.glTranslatef(0.0f, -2.0f, -2.2f);
+
+
+            //save current ModelView Matrix values
+            //in ModelVievMatrixBeforeSpecificTransforms array
+            //ModelView Matrix ========>>>>>> ModelVievMatrixBeforeSpecificTransforms
+            GL.glGetDoublev(GL.GL_MODELVIEW_MATRIX, ModelVievMatrixBeforeSpecificTransforms);
+            //ModelView Matrix was saved, so
+            GL.glLoadIdentity(); // make it identity matrix
+
+            //make transformation in accordance to KeyCode
+            float delta;
+            if (intOptionC != 0)
+            {
+                delta = 5.0f * Math.Abs(intOptionC) / intOptionC; // signed 5
+
+                switch (Math.Abs(intOptionC))
+                {
+                    case 1:
+                        GL.glRotatef(delta, 1, 0, 0);
+                        break;
+                    case 2:
+                        GL.glRotatef(delta, 0, 1, 0);
+                        break;
+                    case 3:
+                        GL.glRotatef(delta, 0, 0, 1);
+                        break;
+                    case 4:
+                        GL.glTranslatef(delta / 20, 0, 0);
+                        break;
+                    case 5:
+                        GL.glTranslatef(0, delta / 20, 0);
+                        break;
+                    case 6:
+                        GL.glTranslatef(0, 0, delta / 20);
+                        break;
+                }
+            }
+            //as result - the ModelView Matrix now is pure representation
+            //of KeyCode transform and only it !!!
+
+            //save current ModelView Matrix values
+            //in CurrentRotationTraslation array
+            //ModelView Matrix =======>>>>>>> CurrentRotationTraslation
+            GL.glGetDoublev(GL.GL_MODELVIEW_MATRIX, CurrentRotationTraslation);
+
+            //The GL.glLoadMatrix function replaces the current matrix with
+            //the one specified in its argument.
+            //The current matrix is the
+            //projection matrix, modelview matrix, or texture matrix,
+            //determined by the current matrix mode (now is ModelView mode)
+            GL.glLoadMatrixd(AccumulatedRotationsTraslations); //Global Matrix
+
+            //The GL.glMultMatrix function multiplies the current matrix by
+            //the one specified in its argument.
+            //That is, if M is the current matrix and T is the matrix passed to
+            //GL.glMultMatrix, then M is replaced with M � T
+            GL.glMultMatrixd(CurrentRotationTraslation);
+
+            //save the matrix product in AccumulatedRotationsTraslations
+            GL.glGetDoublev(GL.GL_MODELVIEW_MATRIX, AccumulatedRotationsTraslations);
+
+            //replace ModelViev Matrix with stored ModelVievMatrixBeforeSpecificTransforms
+            GL.glLoadMatrixd(ModelVievMatrixBeforeSpecificTransforms);
+            //multiply it by KeyCode defined AccumulatedRotationsTraslations matrix
+            GL.glMultMatrixd(AccumulatedRotationsTraslations);
+            Drawings.Draw(pos);
+            GL.glFlush();
+
+            WGL.wglSwapBuffers(m_uint_DC);
+
+        }
+
+        
+        protected virtual void InitializeGL()
+        {
+            m_uint_HWND = (uint)p.Handle.ToInt32();
+            m_uint_DC = WGL.GetDC(m_uint_HWND);
+
+            // Not doing the following WGL.wglSwapBuffers() on the DC will
+            // result in a failure to subsequently create the RC.
+            WGL.wglSwapBuffers(m_uint_DC);
+
+            WGL.PIXELFORMATDESCRIPTOR pfd = new WGL.PIXELFORMATDESCRIPTOR();
+            WGL.ZeroPixelDescriptor(ref pfd);
+            pfd.nVersion = 1;
+            pfd.dwFlags = (WGL.PFD_DRAW_TO_WINDOW | WGL.PFD_SUPPORT_OPENGL | WGL.PFD_DOUBLEBUFFER);
+            pfd.iPixelType = (byte)(WGL.PFD_TYPE_RGBA);
+            pfd.cColorBits = 32;
+            pfd.cDepthBits = 32;
+            pfd.iLayerType = (byte)(WGL.PFD_MAIN_PLANE);
+
+            //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+            //for Stencil support 
+
+            pfd.cStencilBits = 32;
+
+            //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+            int pixelFormatIndex = 0;
+            pixelFormatIndex = WGL.ChoosePixelFormat(m_uint_DC, ref pfd);
+            if (pixelFormatIndex == 0)
+            {
+                MessageBox.Show("Unable to retrieve pixel format");
+                return;
+            }
+
+            if (WGL.SetPixelFormat(m_uint_DC, pixelFormatIndex, ref pfd) == 0)
+            {
+                MessageBox.Show("Unable to set pixel format");
+                return;
+            }
+            //Create rendering context
+            m_uint_RC = WGL.wglCreateContext(m_uint_DC);
+            if (m_uint_RC == 0)
+            {
+                MessageBox.Show("Unable to get rendering context");
+                return;
+            }
+            if (WGL.wglMakeCurrent(m_uint_DC, m_uint_RC) == 0)
+            {
+                MessageBox.Show("Unable to make rendering context current");
+                return;
+            }
+            initRenderingGL();
+        }
+
+        public void OnResize()
+        {
+            Width = p.Width;
+            Height = p.Height;
+            GL.glViewport(0, 0, Width, Height);
+            Draw();
+        }
+
+        protected virtual void initRenderingGL()
+        {
+            if (m_uint_DC == 0 || m_uint_RC == 0)
+                return;
+            if (this.Width == 0 || this.Height == 0)
+                return;
+
+            GL.glShadeModel(GL.GL_SMOOTH);
+            GL.glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+            GL.glClearDepth(1.0f);
+
+
+            GL.glEnable(GL.GL_LIGHT0);
+            GL.glEnable(GL.GL_COLOR_MATERIAL);
+            GL.glColorMaterial(GL.GL_FRONT_AND_BACK, GL.GL_AMBIENT_AND_DIFFUSE);
+
+            GL.glEnable(GL.GL_DEPTH_TEST);
+            GL.glDepthFunc(GL.GL_LEQUAL);
+            GL.glHint(GL.GL_PERSPECTIVE_CORRECTION_Hint, GL.GL_NICEST);
+
+
+            GL.glViewport(0, 0, this.Width, this.Height);
+            GL.glMatrixMode(GL.GL_PROJECTION);
+            GL.glLoadIdentity();
+
+            //nice 3D
+            GLU.gluPerspective(45.0, 1.0, 0.4, 100.0);
+
+
+            GL.glMatrixMode(GL.GL_MODELVIEW);
+            GL.glLoadIdentity();
+
+            //save the current MODELVIEW Matrix (now it is Identity)
+            GL.glGetDoublev(GL.GL_MODELVIEW_MATRIX, AccumulatedRotationsTraslations);
+        }
+    }
+
+}
+
+
+
+
+
